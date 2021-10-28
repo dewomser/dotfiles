a43d2b92ebe1e956fa1f0b97c56f92ebc0436155
Stephanie <stephanie@hey.com>
2021-10-28 11:58:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
-
-# delete existing .zshrc if it already exists
-if [[ -n "${HOME}/.zshrc" ]]; then
-  rm "${HOME}/.zshrc"
-fi
-
-# links
-if [[ -n  "$CODESPACES" ]]; then
-  ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc ~/
-else
-  ln -s ~/.dotfiles/.zshrc ~/
-fi
-
-# Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# delete previous installations
+echo "\e[32m[DOT]\e[34m deleting previous installation of this dotfiles ... \e[39m\n"
+rm -rf ~/.oh-my-zsh/ ~/.zshrc > /dev/null 2>&1
 
 # Install GitHub CLI
 brew install gh
+
+# Reloads the Environment
+test -d ~/.zshrc && source ~/.zshrc
+test -d ~/.profile && source ~/.profile
+
+# removes previous oh-my-zsh
+sudo rm -rf ~/.oh-my-zsh/
+
+# installs oh-my-zsh
+echo "\e[32m[DOT]\e[34m installing oh my zsh ... \e[39m\n"
+! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
+
+echo "\e[32m[DOT]\e[34m copying environment files ... \e[39m\n"
+
+# copies the ZSH environment file
+cp .zshrc ~/.zshrc > /dev/null 2>&1
+
+# touches zsh history file
+[[ ! -f ~/.zsh_history ]] && touch ~/.zsh_history
+
+echo "\e[32mInstallation Finished. Exiting. \e[39m\n"
+
+exit 0
