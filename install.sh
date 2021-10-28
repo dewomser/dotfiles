b364f07beed407545e82d48a784d040ad42e2b7f
Stephanie <stephanie@hey.com>
2021-10-28 11:39:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,11 @@ if [[ -n "${HOME}/.zshrc" ]]; then
   rm "${HOME}/.zshrc"
 fi
 
-# Reload
-test -d ~/.zshrc && source ~/.zshrc
-
 # Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Set ZSH as the default shell
 chsh -s /usr/bin/zsh $(whoami)
 
 # Install GitHub CLI
 brew install gh
-
-echo "\n$(cat .zshrc)" >> ~/.zshrc
