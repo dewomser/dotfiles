62a1686eaa8d82506952e57b7a836d85668d296e
Stephanie <stephanie@hey.com>
2021-10-28 11:50:32 +0200

    sigh codespaces

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
+
+# delete existing .zshrc if it already exists
 if [[ -n "${HOME}/.zshrc" ]]; then
   rm "${HOME}/.zshrc"
 fi
 
+# links
+if [[ -n  "$CODESPACES" ]]; then
+  ln -s /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc ~/
+else
+  ln -s ~/.dotfiles/.zshrc ~/
+fi
+
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Set ZSH as the default shell
-chsh -s /usr/bin/zsh $(whoami)
 
 # Install GitHub CLI
 brew install gh
